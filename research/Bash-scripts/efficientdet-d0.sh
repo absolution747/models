0ac75083af53814e05a0f0a51983c710ef76b03c
Karan Shetty <karan.shetty@wobot.ai>
2021-01-22 16:52:28 +0530

    Bash script update 0.1

--- research/Bash-scripts/efficientdet-d0.sh
+++ research/Bash-scripts/efficientdet-d0.sh
@@ -1,5 +1,4 @@
 wget http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz
 tar -xvzf efficientdet_d0_coco17_tpu-32.tar.gz
-sleep 5
-mv efficientdet_d0_coco17_tpu-32 efficientdet
+cp -a efficientdet_d0_coco17_tpu-32/. efficientdet/
 mv efficientdet/checkpoint efficientdet/checkpoint_start
